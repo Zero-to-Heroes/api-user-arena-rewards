7abf3529c721f94ea3c3dc659fad2a67215e5a26
sebastientromp <sebastien.tromp@gmail.com>
2024-10-13 17:22:16 +0200

    increase batching

--- src/process-save-arena-rewards.ts
+++ src/process-save-arena-rewards.ts
@@ -6,26 +6,25 @@ import { Input } from './sqs-event';
 
 export default async (event, context): Promise<any> => {
 	const events: readonly Input[] = (event.Records as any[])
-		.map(event => JSON.parse(event.body))
+		.map((event) => JSON.parse(event.body))
 		.reduce((a, b) => a.concat(b), [])
-		.filter(event => event);
-	// TODO: do some filtering to keep only the latest for a given userId or userName
+		.filter((event) => event);
+
 	const mysql = await getConnection();
 	for (const ev of events) {
 		await processEvent(ev, mysql);
 	}
+	await mysql.end();
 	const response = {
 		statusCode: 200,
 		isBase64Encoded: false,
 		body: null,
 	};
-	await mysql.end();
 	return response;
 };
 
 const processEvent = async (input: Input, mysql: ServerlessMysql) => {
 	const escape = SqlString.escape;
-	console.debug('handling event', input);
 
 	if (!input.rewards?.length) {
 		console.error('empty rewards', input);
@@ -38,14 +37,14 @@ const processEvent = async (input: Input, mysql: ServerlessMysql) => {
 	}
 
 	const selectQuery = `
-		SELECT * FROM arena_rewards
+		SELECT * FROM arena_rewards_2
 		WHERE runId = ?
 		AND creationDate > DATE_SUB(NOW(), INTERVAL 2 HOUR)
 		LIMIT 1;
 	`;
-	console.debug('running query', selectQuery);
+	// console.debug('running query', selectQuery);
 	const selectResult: any[] = await mysql.query(selectQuery, [input.runId]);
-	console.debug('select result', selectResult);
+	// console.debug('select result', selectResult);
 	if (selectResult && !!selectResult.length) {
 		console.log('already registered rewards for run', input.runId);
 		return;
@@ -53,7 +52,7 @@ const processEvent = async (input: Input, mysql: ServerlessMysql) => {
 
 	for (const reward of input.rewards) {
 		const insertQuery = `
-			INSERT IGNORE INTO arena_rewards
+			INSERT IGNORE INTO arena_rewards_2
 			(
 				creationDate,
 				userId, 
@@ -79,12 +78,12 @@ const processEvent = async (input: Input, mysql: ServerlessMysql) => {
 				${escape(input.currentLosses)}
 			);
 		`;
-		console.log('running query', insertQuery);
+		// console.log('running query', insertQuery);
 		const insertResults: readonly any[] = await mysql.query(insertQuery);
-		console.log(
-			'executed query',
-			insertResults && insertResults.length,
-			insertResults && insertResults.length > 0 && insertResults[0],
-		);
+		// console.log(
+		// 	'executed query',
+		// 	insertResults && insertResults.length,
+		// 	insertResults && insertResults.length > 0 && insertResults[0],
+		// );
 	}
 };
--- src/retrieve-arena-rewards.ts
+++ src/retrieve-arena-rewards.ts
@@ -1,4 +1,4 @@
-import SqlString from 'sqlstring';
+/* eslint-disable no-extra-boolean-cast */
 import { gzipSync } from 'zlib';
 import { getConnection } from './db/rds';
 import { Input } from './sqs-event';
@@ -15,32 +15,22 @@ export default async (event): Promise<any> => {
 	const input: Input = JSON.parse(event.body);
 	console.debug('handling event', input);
 
-	const mysql = await getConnection();
-	const escape = SqlString.escape;
-	const userIds = await getAllUserIds(input.userId, input.userName, mysql);
-	console.debug('retrieved user ids', userIds);
-
-	// First-time user, no mapping registered yet
-	if (!userIds?.length) {
-		await mysql.end();
-		return {
-			statusCode: 200,
-			isBase64Encoded: false,
-			body: JSON.stringify({ results: [] }),
-		};
-	}
-
-	// Limit the time frame to reduce the query time + reduce the data sent to the user
-	// Since Firestone only shows you rather recent data, there is no point in getting data that is a year old
-	const existingQuery = `
+	let query = `
 		SELECT * 
-		FROM arena_rewards
+		FROM arena_rewards_2
 		WHERE creationDate >= DATE_SUB(NOW(), INTERVAL 3 MONTH)
-		AND userId IN (${escape(userIds)})
+		AND userName = ?
 	`;
-	console.debug('getting rewards', existingQuery);
-	const results: readonly ArenaRewardInfo[] = await mysql.query(existingQuery);
-	console.debug('got rewards from db', results?.length);
+	if (!input?.userName) {
+		query = `
+			SELECT *
+			FROM arena_rewards_2
+			WHERE creationDate >= DATE_SUB(NOW(), INTERVAL 3 MONTH)
+			AND userId = ?
+		`;
+	}
+	const mysql = await getConnection();
+	const results: readonly ArenaRewardInfo[] = await mysql.query(query, [input.userName || input.userId]);
 	await mysql.end();
 
 	const stringResults = JSON.stringify(results);
@@ -56,27 +46,6 @@ export default async (event): Promise<any> => {
 	};
 
 	return response;
-};
-
-const getAllUserIds = async (userId: string, userName: string, mysql): Promise<readonly string[]> => {
-	const escape = SqlString.escape;
-	const userSelectQuery = `
-			SELECT DISTINCT userId FROM user_mapping
-			INNER JOIN (
-				SELECT DISTINCT username FROM user_mapping
-				WHERE 
-					(username = ${escape(userName)} OR username = ${escape(userId)} OR userId = ${escape(userId)})
-					AND username IS NOT NULL
-					AND username != ''
-					AND username != 'null'
-					AND userId != ''
-					AND userId IS NOT NULL
-					AND userId != 'null'
-			) AS x ON x.username = user_mapping.username
-			UNION ALL SELECT ${escape(userId)}
-		`;
-	const userIds: any[] = await mysql.query(userSelectQuery);
-	return userIds.map((result) => result.userId);
 };
 
 export interface ArenaRewardInfo {
